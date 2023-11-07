a6c97f3d4f2db72ca14f3f3c5c67594484472163
Myllena Souza <mssilva.egq@uesc.br>
2023-11-07 17:08:39 -0300

    Primeiro commit

---
+++ Semana1/MeuProjeto/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region TiposDados
+    int tipoInteiro;
+    double tipoDouble;
+    decimal tipoDecimal;
+    long tipoLong;
+#endregion
