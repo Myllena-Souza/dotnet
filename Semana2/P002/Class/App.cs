6e86525f5ddb1da60ac7efb1084f70539bfa9853
Myllena <mssilva.egq@uesc.br>
2023-11-19 10:28:07 -0300

    criação da class app

---
+++ Semana2/P002/Class/App.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Runtime.InteropServices;
+
+namespace P002;
+
+public class App
+{
+    private List<Tarefa> Tarefas = new List<Tarefa>();
+
+    private void Criar (){
+        System.Console.WriteLine("Digite o titulo da tarefa: ");
+        string titulo = Console.ReadLine();
+        System.Console.WriteLine("Digite uma descrição: ");
+        string descricao = Console.ReadLine();
+        System.Console.WriteLine("Digite o dia: ");
+        int dia = int.Parse (Console.ReadLine());
+        System.Console.WriteLine("Digite o mês: ");
+        int mes = int.Parse (Console.ReadLine());
+        System.Console.WriteLine("Digite o ano: ");
+        int ano = int.Parse (Console.ReadLine());
+        System.Console.WriteLine("A tarefa foi concluida? (S/N): ");
+        Boolean entradaConc;
+        if (aux == "s")
+        {
+            entradaConc = true;
+        }
+        else
+        {
+            entradaConc = false;
+        }
+
+        DateTime data = new DateTime(ano, mes, dia);
+        Tarefa tarefa = new Tarefa(titulo, descricao, data, entradaConc);
+        this.Tarefas.Add (tarefa);
+
+    }
+
+}
---
+++ Semana2/P002/Class/Tarefa.cs
@@ -0,0 +1,26 @@
+﻿namespace P002;
+
+public class Tarefa
+{
+    string Titulo;
+    string Descricao;
+    DateTime DataVencimento;
+    Boolean Conclusao;
+
+    public Tarefa(string _titulo, string _descricao, DateTime _dataVenc, Boolean _conclusao)
+    {
+        this.Titulo = _titulo;
+        this.Descricao = _descricao;
+        this.DataVencimento = _dataVenc;
+        this.Conclusao = _conclusao;
+    }
+
+    public void Listar()
+    {
+        Console.WriteLine ("Titulo: " + this.Titulo);
+        Console.WriteLine ("Descricao: " + this.Descricao);
+        Console.WriteLine ("Titulo: " + this.DataVencimento);
+    }
+
+
+}
