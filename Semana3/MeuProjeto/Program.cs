c4f85d4d20e530617a6b3d4d63bfba99df10dd22
Myllena Souza <mssilva.egq@uesc.br>
2023-11-21 14:12:18 -0300

    atividades da semana 3

---
+++ Semana3/MeuProjeto/Program.cs
@@ -0,0 +1,7 @@
+﻿#region 
+using System.IO.Compression;
+
+var tuple3 = (id: 10,nome: "Myllena", born: new DateTime(1996, 1, 23));
+Console.WriteLine($"tuple 3: {tuple3.id}, {tuple3.nome}, {tuple3.born}");
+#endregion
+
